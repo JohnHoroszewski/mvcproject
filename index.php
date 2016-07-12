f2fab028b33becd323814c813a0cb6866cd83d9f
John Horoszewski <j.horoszewski@gmail.com>
2016-07-12 09:33:57 -0400

    * Initial setup of Router for routing * Created .htaccess for vanity links

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
-    echo 'Howdy from the Home Folder!!!!!';
-?>
+
+/**
+ *  Front Controller
+ */
+
+//echo 'Requested URL = "' . $_SERVER['QUERY_STRING'] . '"';
+
+/**
+ * Routing
+ */
+require 'Core/Router.php';
+
+$router = new Router();
+
+echo get_class($router);
